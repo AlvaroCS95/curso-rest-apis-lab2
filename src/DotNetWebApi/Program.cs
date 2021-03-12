2d9f5645513c5620e4039846e79fc78082b3e1f0
Alvaro Carranza <alvarocarranza95@gmail.com>
2021-03-11 21:33:51 -0600

    Add files via upload
    Lab 2

---
+++ src/DotNetWebApi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+
+WebHost.CreateDefaultBuilder().Configure(app =>
+{
+    app.UseRouting();
+    app.UseEndpoints(e =>
+    {
+        e.MapGet("/", c => c.Response.WriteAsync("Hello world!"));
+        e.MapGet("hello", c => c.Response.WriteAsync("Hello, visitor!"));
+        e.MapGet("hello/{name}", context => {
+
+            context.Response.WriteAsync($"Hello, {context.Request.RouteValues["name"]}");
+            return System.Threading.Tasks.Task.CompletedTask;
+
+        });
+
+        
+        e.MapGet("hellox/{name}", context => 
+
+            context.Response.WriteAsJsonAsync(new {Message="Hello, visitante"})
+            
+
+        );
+        //e.MapGet("hello/{name}", c => c.Response.WriteAsync($"Hello, {c.Request.RouteValues["name"]}"));
+
+       e.MapPost("hellos", async context => {
+
+           var value = new StringValues(context.Request.Host.Host);
+         //  byte[] buffer = new byte[context.Request.Body.Length]; 
+         //  var content = await context.Request.Body.ReadAsync(buffer,0,(int) context.Request.Body.Length);
+
+         using var reader = new StreamReader (context.Request.Body,System.Text.Encoding.UTF8);
+         var content = await reader.ReadToEndAsync();
+
+            context.Response.Headers.Add("Host-name",value);
+            
+            System.Console.WriteLine(content);
+            context.Response.StatusCode=200;
+
+     });
+        
+    });
+}).Build().Run();
+
+
+/*
+{}
+curso-rest-apis-lab2
+*/
